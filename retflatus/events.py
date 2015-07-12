c57e95fcf7db7bf5ceaf32ee584b019333a78a5e
stephenhsu <stephenhsu90@gmail.com>
2015-07-12 22:13:37 +0800

    add zmq listener and handler

---
+++ retflatus/events.py
@@ -0,0 +1,443 @@
+"""
+This events.py focuses on listening and handling events
+"""
+import zmq
+import threading
+from six.moves import queue as Queue
+from utils import StoppedException, setup_logging
+import logging
+import json
+from abc import ABCMeta, abstractmethod
+
+
+STATUS_MAP = {"SUCCESS": "success",
+              "FAILURE": "failure",
+              "ABORTED": "aborted"
+              }
+
+
+class ZMQListener(threading.Thread):
+    """
+    a wrapped class to listen zmq events from Jenkins
+    """
+    log = logging.getLogger('events.ZMQListener')
+
+    def __init__(self, name, addr, jenkinsmgr, flows):
+        """
+        @param name: the name of the zmq
+        @param addr: the address of the zmq
+        @param jenkinsmgr: JenkinsManager instance
+        @param flows: flows object
+        """
+        threading.Thread.__init__(self, name=name)
+        self.addr = addr
+        self.name = name
+        self._context = zmq.Context()
+        self.socket = self._context.socket(zmq.SUB)
+        self._stopped = False
+        self.handler = EventsHandler('%s-handler' % self.name,
+                                     jenkinsmgr,
+                                     flows)
+
+    def run(self):
+        self._setup_socket()
+        self.handler.start()
+        self.log.debug('ZMQListenner %s Starts Listening' % self.name)
+        while not self._stopped:
+            event = self.socket.recv().decode('utf-8')
+            self.handler.submitEvent(event)
+            self.log.debug(event)
+
+    def stop(self):
+        self._stopped = True
+        if self._context:
+            self.log.debug('ZMQListenner %s Stops Listening' % self.name)
+            self._context.destroy()
+
+    def _setup_socket(self):
+        self.log.debug('Setup Socket for ZMQListenner %s' % self.name)
+        self.socket.connect(self.addr)
+        self.socket.setsockopt(zmq.SUBSCRIBE, '')
+
+
+class EventsHandler(threading.Thread):
+    """
+    events handler
+    """
+    log = logging.getLogger("events.EventsHandler")
+
+    def __init__(self, name, jenkinsmgr, flows):
+        threading.Thread.__init__(self, name=name)
+        self.queue = Queue.Queue()
+        self.lock = threading.Lock()
+        self.name = name
+        self.jenkinsmgr = jenkinsmgr
+        self.flows = flows
+        self._stopped = False
+
+    def run(self):
+        self.log.debug('Handler %s Starts Handling Events' % self.name)
+        while not self._stopped:
+            event = self.queue.get()
+            if not event:
+                continue
+            self.handle_event(event)
+
+    def stop(self):
+        self._stopped = True
+        self.queue.put(None)
+
+    def submitEvent(self, event):
+        if self._stopped:
+            raise StoppedException("Handler %s is no longer running"
+                                   % self.name)
+        self.queue.put(event)
+
+    def handle_event(self, event):
+        topic, data = event.split(None)
+
+        if topic == 'onStarted':
+            self._handle_started_event(data)
+        elif topic == 'onCompleted':
+            pass
+        elif topic == 'onFinalized':
+            self._handle_finalized_event(data)
+
+    def _handle_started_event(self, data):
+        """
+        handle started event
+        """
+        event_thread = StartedEventThread(data,
+                                          self.jenkinsmgr,
+                                          self.flows,
+                                          self.lock)
+        event_thread.start()
+        if event_thread.isrootflow:
+            self.log.debug("Waiting for cleanup.")
+            event_thread.join()
+
+    def _handle_finalized_event(self, data):
+        """
+        handle finalized event
+        """
+        event_thread = FinalizedEventThread(data,
+                                            self.jenkinsmgr,
+                                            self.flows,
+                                            self.lock)
+        event_thread.start()
+
+
+class EventThread(threading.Thread):
+    """
+    thread to handle single event
+    """
+    __metaclass__ = ABCMeta
+    log = logging.getLogger("events.EventThread")
+
+    def __init__(self, data, jenkinsmgr, flows, lock):
+        self.data = json.loads(data)
+        super(EventThread, self).__init__(name=self.data["name"])
+        self.name = self.data["name"]
+        self.build = self.data["build"]
+        self.jenkinsmgr = jenkinsmgr
+        self.flows = flows
+        self.lock = lock
+
+    @abstractmethod
+    def run(self):
+        pass
+
+    def getCauses(self):
+        """
+        get the causes of a certain build
+        @return: causes list that containing upstream build names
+                 None for empty list
+        """
+        return self.jenkinsmgr.getRootCauses(self.name,
+                                             self.build["number"])
+
+    @property
+    def isflow(self):
+        """
+        identify whether the event is a flow event
+        @return: True or False
+        """
+        if not hasattr(self, "_isflow"):
+            self._isflow = self.jenkinsmgr.is_flow(self.name)
+        return self._isflow
+
+    @property
+    def isrootflow(self):
+        """
+        identify whether the event is a root flow event
+        """
+        if not hasattr(self, "_isrootflow"):
+            if self.isflow and not self.causes:
+                self._isrootflow = True
+            else:
+                self._isrootflow = False
+        return self._isrootflow
+
+    @property
+    def causes(self):
+        if not hasattr(self, "_causes"):
+            self._causes = self.getCauses()
+        return self._causes
+
+    @property
+    def status(self):
+        """
+        event status
+        """
+        if not hasattr(self, "_status"):
+            self._status = self._getStatus()
+        return self._status
+
+    @abstractmethod
+    def _getStatus(self):
+        """
+        get the status from the event
+        """
+        pass
+
+    def checkEventOutdated(self):
+        """
+        check whether the job event is outdated
+        only need to check for job/subflow event
+        @return: True for outdated, otherwise False
+        """
+        self.log.debug("check whether Event <%s> is out-dated." % self.name)
+        outdated_msg = "The Event <%s> is out-dated. Ignore it." % self.name
+
+        if self.isrootflow:
+            # actually there is no need to check for root flow event
+            # keep it for consistent and future use
+            flow = self.flows.get(self.name, None)
+            if not flow:
+                # return True for None
+                self.log.debug(" ".join(["Unable to find Flow",
+                                         "<%s> in" % self.name,
+                                         "configuration file"]))
+                return True
+
+            new_buildno = int(self.build["number"])
+            current_buildno = int(flow.build["number"])
+            if new_buildno < current_buildno:
+                self.log.debug(outdated_msg)
+                return True
+            return False
+        else:
+            upstream_flow = self.causes[0]
+            upstream_flowname = upstream_flow.upstreamProject
+            upstream_flowno = upstream_flow.upstreamBuild
+            self.log.info("upstream %s: %s" % (upstream_flowname,
+                                               upstream_flowno))
+            try:
+                current_flow = self.flows.get(upstream_flowname)
+                current_flowno = current_flow.build["number"]
+                self.log.info("current %s: %s" % (upstream_flowname,
+                                                  current_flowno))
+            except:
+                self.log.error("Missing Flow <%s> Info." % upstream_flowname)
+                return False
+            if upstream_flowno < current_flowno:
+                self.log.info("%s < %s" % (upstream_flowno,
+                                           current_flowno))
+                self.log.info("type: %s" % type(current_flowno))
+                self.log.debug(outdated_msg)
+                return True
+            else:
+                return False
+
+    def updateStatus(self):
+        """
+        update Event status
+        """
+        if self.causes:
+            # subflows or jobs
+            self._updateJobStatus()
+        else:
+            # no causes jobs
+            if not self.isflow:
+                self.log.debug("Job <%s> has no upstream flow." % self.name)
+                return
+
+            # uppermost flow
+            self._updateFlowStatus()
+
+    def _updateJobStatus(self):
+        """
+        update job status
+        """
+        upstream_flow = self.causes[0]
+        upstreamProject = upstream_flow.upstreamProject
+
+        try:
+            jobs = self.flows[upstreamProject].jobs
+        except KeyError:
+            self.log.error(" ".join(["Unable to find Job",
+                                     "<%s>'s upstream" % self.name,
+                                     "Project <%s>" % upstreamProject,
+                                     "in the configuration file."]))
+            return
+        except AttributeError:
+            self.log.error(" ".join(["Job <%s>'s upstream" % self.name,
+                                     "Project <%s> has" % upstreamProject,
+                                     "no jobs in the",
+                                     "configuration file."]))
+            return
+
+        event_jobs = self._findJobs(jobs)
+        if not event_jobs:
+            return
+
+        with self.lock:
+            self.log.debug("lock acquire job")
+            # check outdated
+            if self.checkEventOutdated():
+                return
+
+            # update status
+            parameters = self.build.get("parameters", None)
+            for event_job in event_jobs:
+                identifiers = event_job.identifier
+                if self._isMatched(identifiers, parameters):
+                    event_job.build = self.build
+                    event_job.status = self.status
+
+                    self.log.debug(" ".join(["Successfully Update Job",
+                                             "<%s> status" % self.name
+                                             ]))
+                    break
+
+            else:
+                self.log.error(" ".join(["No matched Job <%s> " % self.name,
+                                         "in configuration file"]))
+
+    def _updateFlowStatus(self):
+        """
+        update flow status
+        """
+        with self.lock:
+            self.log.debug("lock acquire flow")
+            flow = self.flows.get(self.name, None)
+            if flow:
+                self._cleanupFlowStatus()
+                flow.build = self.build
+                flow.status = self.status
+                self.log.debug(" ".join(["Successfully Update Flow",
+                                         "<%s> status" % self.name
+                                         ]))
+            else:
+                self.log.debug(" ".join(["Unable to find Flow",
+                                         "<%s> in" % self.name,
+                                         "configuration file"]))
+
+    def _cleanupFlowStatus(self):
+        """
+        cleanup the build status and info
+        Mainly for "STARTED" event
+        """
+        pass
+
+    def _findJobs(self, jobs):
+        """
+        find jobs that matches job_name
+        @param jobs: the jobs config
+        @return: jobs list
+        """
+        jobs_list = list()
+
+        if not hasattr(jobs, "__iter__"):
+            if jobs.name == self.name:
+                jobs_list.append(jobs)
+                return jobs_list
+            return None
+
+        for job in jobs:
+            job_list = self._findJobs(job)
+            if job_list:
+                jobs_list.extend(job_list)
+
+        return jobs_list if jobs_list else None
+
+    def _isMatched(self, identifiers, parameters):
+        """
+        check whether the job's identifiers match job's triggered parameters
+        """
+        if identifiers is None or not parameters:
+            return True
+
+        flag = False
+        for (key, value) in identifiers.iteritems():
+            if value != parameters.get(key):
+                break
+        else:
+            flag = True
+
+        return flag
+
+
+class StartedEventThread(EventThread):
+    log = logging.getLogger("events.StartedEventThread")
+
+    def _getStatus(self):
+        return "running"
+
+    def _cleanupFlowStatus(self):
+        flow = self.flows.get(self.name, None)
+        try:
+            jobs = flow.jobs
+        except AttributeError:
+            self.log.error(" ".join(["Flow <%s> has" % self.name,
+                                     "no jobs in the",
+                                     "configuration file."]))
+            return
+        flow.build = None
+        flow.status = None
+        jobs_list = self._getJobsList(jobs)
+        for job in jobs_list:
+            job.build = None
+            job.status = None
+        self.log.debug("Successfully cleanup all the downstream jobs.")
+        return
+
+    def _getJobsList(self, jobs):
+        """
+        get jobs list from jobs config
+        @param jobs: the jobs config
+        @return: jobs list
+        """
+        jobs_list = list()
+
+        if not hasattr(jobs, "__iter__"):
+            jobs_list.append(jobs)
+            return jobs_list
+
+        for job in jobs:
+            job_list = self._getJobsList(job)
+            if job_list:
+                jobs_list.extend(job_list)
+
+        return jobs_list if jobs_list else None
+
+    def run(self):
+        self.log.info("Start to Update Flow/Job <%s> Status" % self.name)
+        self.updateStatus()
+
+
+class FinalizedEventThread(EventThread):
+    log = logging.getLogger("events.FinalizedEventThread")
+
+    def _getStatus(self):
+        status = STATUS_MAP[self.build["status"]]
+        return status
+
+    def run(self):
+        self.log.info("Start to Update Flow/Job <%s> Status" % self.name)
+        self.updateStatus()
+
+if __name__ == "__main__":
+    setup_logging()
+    zmql = ZMQListener('local_zmq', 'tcp://localhost:8888', None, None)
+    zmql.start()
---
+++ retflatus/loader.py
@@ -0,0 +1,248 @@
+"""
+load/parse flow yaml file
+"""
+import yaml
+import utils
+import logging
+
+
+class Serial(list):
+    """
+    used to mark up serial jobs
+    """
+    def __str__(self):
+        return 'Serial(%s)' % super(Serial, self).__str__()
+
+
+class Parallel(list):
+    """
+    used to mark up parallel job
+    """
+    def __str__(self):
+        return 'parallel(%s)' % super(Parallel, self).__str__()
+
+
+class FlowConfig(utils.ConfigInfo):
+    def __repr__(self):
+        return "<Flow {0.name}>".format(self)
+
+
+class JobConfig(utils.ConfigInfo):
+    def __repr__(self):
+        return "<Job {0.name} 0x{1:x}>".format(self, id(self))
+
+
+class Loader(object):
+    """
+    Read and reshape conf
+    """
+    log = logging.getLogger('loader.Loader')
+
+    def __init__(self, path):
+        """
+        @param path: the path of the yaml file
+        """
+        self.path = path
+        self.original_data = None
+
+    def getConfig(self):
+        self.read()
+        return self.conf.reshaped_flows, self.conf.flows_map
+
+    def read(self):
+        self.log.info("Read conf file %s" % self.path)
+        self._read()
+        self.reshape()
+        self.getMap()
+
+    def _read(self):
+        """
+        read the native conf file into a well-constructed format
+        """
+        self.original_data = yaml.load(open(self.path))
+        self.conf = utils.ConfigInfo()
+        self.conf.flows = {}
+        flows = self.original_data.get('flows', None)
+
+        if not flows:
+            return None
+
+        for flow in flows:
+            f = FlowConfig()
+            f.name = flow['name']
+
+            if f.name in self.conf.flows.keys():
+                self.log.warning(" ".join(["There already exists a same flow",
+                                           "name <%s> in your" % f.name,
+                                           "configuration file."]))
+                self.log.warning(" ".join(["If you do want to override it,",
+                                           "please ignore this warning"]))
+
+            f.label = flow.get('label', False)
+            if f.label:
+                labeledBy = f.name
+            else:
+                labeledBy = None
+            f.jobs = self._read_jobs(flow.get('jobs'), labeledBy)
+            self.conf.flows[f.name] = f
+
+    def _read_jobs(self, jobs, labeledBy=None):
+        """
+        read jobs info
+        """
+        jobs_list = Serial()
+        for job in jobs:
+            job_type = job.keys()[0]
+
+            if job_type == "serial":
+                job_list = Serial()
+                subjobs = job.get('serial')
+
+            if job_type == "parallel":
+                job_list = Parallel()
+                subjobs = job.get('parallel')
+
+            for subjob in subjobs:
+                sj = JobConfig()
+                sj.name = subjob.get('name')
+                sj.description = subjob.get('description', None)
+                sj.label = subjob.get('label', False)
+                sj.identifier = subjob.get('identifier', None)
+                sj.labeledBy = labeledBy
+                job_list.append(sj)
+
+            jobs_list.append(job_list)
+        return jobs_list
+
+    def reshape(self):
+        """
+        substitute and remove labeled flows
+        """
+        flows = self.conf.flows
+        reshaped_flows = {}
+        for (flow_name, flow_info) in flows.iteritems():
+            if flow_info.getattr('label'):
+                continue
+            f = FlowConfig()
+            f.name = flow_name
+            f.jobs = self._reshape(flow_name)
+            reshaped_flows[f.name] = f
+        self.conf.reshaped_flows = reshaped_flows
+
+    def _reshape(self, flow_name):
+        """
+        TBD: only support flows with single serial or parallel label
+        """
+        try:
+            jobs = self.conf.flows[flow_name].jobs
+            reshaped_jobs = Serial()
+            for job in jobs:
+
+                job_serial = self.is_serial(job)
+                if job_serial:
+                    job_list = Serial()
+                else:
+                    job_list = Parallel()
+
+                for subjob in job:
+                    if subjob.getattr('label'):
+                        labeled_jobs = self.conf.flows[subjob.name].jobs
+                        if len(labeled_jobs) == 1:
+                            labeled_job = labeled_jobs[0]
+                            labeled_job_serial = self.is_serial(labeled_job)
+
+                            if job_serial == labeled_job_serial:
+                                job_list.extend(labeled_job)
+                            else:
+                                job_list.append(labeled_job)
+
+                        else:
+                            self.log.error(" ".join(["Unsupported Structure.",
+                                                     "Will support later."]))
+                            return None
+                    else:
+                        job_list.append(subjob)
+
+                reshaped_jobs.append(job_list)
+            return reshaped_jobs
+        except Exception, excp:
+            self.log.error("Unable to reshape flow %s" % flow_name)
+            self.log.error(excp)
+            return None
+
+    def getMap(self):
+        """
+        Generate flow map used for plotting in the front-end
+        """
+        flows = self.conf.reshaped_flows
+        flow_map = dict()
+        for (flow_name, flow_info) in flows.iteritems():
+            flow_map[flow_name] = self.generateMap(flow_info.jobs)
+        self.conf.flows_map = flow_map
+
+    def generateMap(self, jobs):
+        """
+        Generate map through jobs list
+        """
+        return self._generateMap(jobs)[0]
+
+    def _generateMap(self, jobs, previous=None):
+
+        jobs_map = dict()
+        jobs_serial = self.is_serial(jobs)
+
+        if not jobs_serial:
+            previous_list = list()
+
+        for job in jobs:
+            if not isinstance(job, JobConfig):
+                (job_map, subprevious) = self._generateMap(job,
+                                                           previous)
+                jobs_map.update(job_map)
+
+                if jobs_serial:
+                    previous = subprevious
+                else:
+                    previous_list.extend(subprevious)
+
+                continue
+
+            if previous:
+                job.previous = previous
+
+            job_id = str(job)
+            jobs_map[job_id] = job
+            if jobs_serial:
+                previous = [job_id]
+            else:
+                previous_list.append(job_id)
+
+        if not jobs_serial:
+            previous = previous_list
+
+        return jobs_map, previous
+
+    def is_serial(self, job):
+        """
+        check whether the job is a serial job
+        """
+        if isinstance(job, Serial):
+            return True
+        return False
+
+    def is_parallel(self, job):
+        """
+        check whether the job is a parallel job
+        """
+        if isinstance(job, Parallel):
+            return True
+        return False
+
+
+if __name__ == "__main__":
+    from utils import setup_logging
+    setup_logging()
+    filepath = './conf/flows.yaml'
+    c = Loader(filepath)
+    d = c.getConfig()
+    print d
